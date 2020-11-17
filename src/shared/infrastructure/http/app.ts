0b320549099dd241217c5162163d2665bed3f7cc
alexandredev3 <costasalexandre511@gmail.com>
2020-11-17 18:00:50 -0300

    app: added rate limiter

--- src/shared/infrastructure/http/app.ts
+++ src/shared/infrastructure/http/app.ts
@@ -4,8 +4,11 @@ import '@shared/infrastructure/typeorm';
 import '@shared/container';
 
 import routes from '@shared/infrastructure/http/routes';
+import rateLimiter from '@shared/infrastructure/http/middlewares/rateLimiter';
 
 export const app = express();
 
 app.use(express.json());
+
+app.use(rateLimiter);
 app.use(routes);
---
+++ src/shared/infrastructure/http/middlewares/rateLimiter.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+import { createClient } from 'redis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+
+import {
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_PASSWORD
+} from '@shared/utils/environment';
+
+const redisClient = createClient({
+  host: REDIS_HOST,
+  port: Number(REDIS_PORT),
+  password: REDIS_PASSWORD || undefined
+});
+
+/**
+ * Quem estiver consumindo minha API tem direto de fazer 4 requests em 1 segundo,
+ * se for mais de 4 requets em 1 segundo a API vai bloqueiar.
+ */
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'rateLimiter', // chave unica, que contem essas opções.
+  points: 4, // 4 pontos
+  duration: 1 // por 1 segundo.
+});
+
+async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    // salvando o ip do consumidor da minha API.
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch(error) {
+    return response.status(429).json({
+      error: 'Too many request'
+    })
+  }
+}
+
+export default rateLimiter;
